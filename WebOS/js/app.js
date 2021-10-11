0251ba71090003308332f072908421db85769e0d
Subash <83503341+Subash-A-A@users.noreply.github.com>
2021-10-11 19:19:16 +0530

    Add files via upload

--- WebOS/js/app.js
+++ WebOS/js/app.js
@@ -1,4 +1,17 @@
 function display()
-{
-    
+{   
+    let div = document.getElementById("start__popup");
+    div.style.opacity = 1;
+}
+function popoff(){
+    let div = document.getElementById("start__popup");
+    div.style.opacity = 0;
+}
+function popoff__myComp(){
+    let div = document.getElementById("my__computer");
+    div.style.opacity = 0;
+}
+function display__myComp(){
+    let div = document.getElementById("my__computer");
+    div.style.opacity = 1;
 }
